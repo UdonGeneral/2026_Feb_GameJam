052e63552341c227b7ac3e93d02bac7de3272aee
HW25A005 <hw25a005@oecu.jp>
2026-04-18 03:05:57 +0900

    ishii/characterScriptをコミット
    移動と前進時のアニメーション制御を実装しました
    後退時についてはゲーム的に後退することがあるのか分からなかったので一旦未実装です

---
+++ UnityData/Assets/Ishii/characterScript/Move/Script/MoveAnimation.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+
+using UnityEngine;
+
+public class MovementAndAnimation
+ : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public float decelerationRate = 5f; // 止まる時の滑り具合（小さいほどツルツル滑る）
+    public float accelerationRate = 10f; // 動き出しの機敏さ（大きいほどすぐ最高速になる）
+
+    private float currentVelocityX = 0f; //向きと速さを記録
+    public bool canMove = true;
+
+    private Animator animator;
+
+    // 現在ボタンを押して移動しようとしているかどうかの状態
+    private bool isInputMoving = false;
+    void Start()
+    {
+        Time.timeScale = 1f;
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        bool hasInput = Mathf.Abs(moveX) > 0.01f;
+
+        if (!canMove)
+        {
+            // 徐々に減速
+            currentVelocityX = Mathf.Lerp(currentVelocityX, 0f, decelerationRate * Time.deltaTime);
+            Debug.Log("66");
+        }
+        else
+        {
+            if (hasInput)
+            {
+                // ボタンを押している時：目標の速度に向かって加速
+                float targetVelocity = moveX * moveSpeed;
+                currentVelocityX = Mathf.Lerp(currentVelocityX, targetVelocity, accelerationRate * Time.deltaTime);
+            }
+            else
+            {
+                // ボタンを離した時：0 に向かってゆっくり減速
+                currentVelocityX = Mathf.Lerp(currentVelocityX, 0f, decelerationRate * Time.deltaTime);
+            }
+        }
+
+        bool isActuallyMoving = Mathf.Abs(currentVelocityX) > 0.1f;
+
+        if (hasInput && !isInputMoving && canMove)
+        {
+            OnStartMoving();
+            isInputMoving = true;
+        }
+
+        // 「今は入力がない」かつ「さっきまでは動いていた」
+        else if (!hasInput && isInputMoving)
+        {
+            OnStopMoving();
+            isInputMoving = false;
+        }
+
+
+        Vector3 move = new Vector3(currentVelocityX, 0, 0);
+
+        transform.Translate(move * Time.deltaTime, Space.World);
+    }
+
+    public void StopMoving()
+    {
+
+        canMove = false;
+        if (isInputMoving)
+        {
+            OnStopMoving();
+            isInputMoving = false;
+        }
+
+    }
+
+    private void OnStartMoving()
+    {
+        Debug.Log("動いた");
+        animator.SetBool("is_walking", true);
+    }
+
+    private void OnStopMoving()
+    {
+        Debug.Log("止まった");
+        animator.SetBool("is_walking", false);
+    }
+}
